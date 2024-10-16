80de543f0480964033b4973cfd1e3bbf79ccec36
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-10-16 09:31:38 +0500

    add role, userimage models to db and change db itself to sqlite

--- app/auth/database.py
+++ app/auth/database.py
@@ -1,27 +1,54 @@
+import datetime
+import enum
 from typing import AsyncGenerator
 
 from fastapi import Depends
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
-from sqlalchemy import String, Boolean, Integer
+from sqlalchemy import String, Boolean, Integer, Enum, ForeignKey, TIMESTAMP
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
-from app.config import DB_USER, DB_PASS, DB_HOST, DB_NAME, DB_PORT
 
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 
 
 class Base(DeclarativeBase):
     pass
 
 
+class Role(enum.Enum):
+    interviewer = "interviewer"
+    candidate = "candidate"
+
+
+class UserImage(Base):
+    __tablename__ = "user_image"
+
+    id: Mapped[int] = mapped_column(Integer, unique=True, index=True, nullable=False, primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("user.id"), nullable=False)
+    imageUrl: Mapped[str] = mapped_column(String, nullable=True)
+
+    user: Mapped["User"] = relationship("User", back_populates="image")
+
+
 class User(SQLAlchemyBaseUserTable[int], Base):
     id: Mapped[int] = mapped_column(Integer, unique=True, index=True, nullable=False, primary_key=True)
+    fullName: Mapped[str] = mapped_column(String(length=255), nullable=False)
     email: Mapped[str] = mapped_column(String(length=320), unique=True, index=True, nullable=False)
     hashed_password: Mapped[str] = mapped_column(String(length=1024), nullable=False)
+    role: Mapped[Role] = mapped_column(Enum(Role), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
     is_superuser: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
     is_verified: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+
+    imageUrl: Mapped[str] = mapped_column(String, nullable=True)
+    image: Mapped["UserImage"] = relationship("UserImage", uselist=False, back_populates="user")
+
+    created_at: Mapped[datetime.datetime] = mapped_column(TIMESTAMP(timezone=True),
+                                                          default=lambda: datetime.datetime.now(datetime.timezone.utc))
+    updated_at: Mapped[datetime.datetime] = mapped_column(TIMESTAMP(timezone=True),
+                                                          default=lambda: datetime.datetime.now(datetime.timezone.utc),
+                                                          onupdate=lambda: datetime.datetime.now(datetime.timezone.utc))
 
 
 engine = create_async_engine(DATABASE_URL)
