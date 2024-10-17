5336786e0faa882acc07ce4245e6172b95a245f2
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-10-17 08:54:04 +0500

    include user image upload api

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
+from app.auth.auth_backend import image_router as auth_image_router
 
 router = APIRouter()
+
+router.include_router(auth_image_router, prefix="/auth/image", tags=["auth"])
 
 
