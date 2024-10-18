0bfddba90bd99c0abfae1e1391af41ecbf867dc2
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-10-18 22:13:28 +0500

    include router of question and questiontype

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter
 from app.auth.auth_backend import image_router as auth_image_router
+from app.question.api import router as question_router
+from app.question.api import router_type as question_type_router
 
 router = APIRouter()
 
 router.include_router(auth_image_router, prefix="/auth/image", tags=["auth"])
+router.include_router(question_router, prefix="/question", tags=["Question"])
+router.include_router(question_type_router, prefix="/question/type", tags=["Question Type"])
 
 
