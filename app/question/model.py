bf37d0c6f7d5b8a159e6ebf7319b64f6621af91a
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-10-22 11:20:09 +0500

    correct relationships

--- app/question/model.py
+++ app/question/model.py
@@ -1,6 +1,6 @@
 import datetime
 
-from sqlalchemy import Integer, String, TIMESTAMP
+from sqlalchemy import Integer, String, TIMESTAMP, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.auth.database import Base
 
@@ -9,7 +9,7 @@ class QuestionType(Base):
     __tablename__ = 'question_type'
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    typeName: Mapped[str] = mapped_column(String(255), nullable=False)
+    typeName: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
     questions: Mapped[list["Question"]] = relationship("Question", back_populates="type", lazy="selectin")
 
     created_at: Mapped[datetime.datetime] = mapped_column(TIMESTAMP(timezone=True),
@@ -26,8 +26,8 @@ class Question(Base):
     text: Mapped[str] = mapped_column(String(255), nullable=False)
     answer: Mapped[str] = mapped_column(String(255), nullable=False)
 
-    type_id: Mapped[int] = mapped_column(Integer, nullable=False)
-    type: Mapped[list["QuestionType"]] = relationship("QuestionType", back_populates="questions", lazy="selectin")
+    type_id: Mapped[int] = mapped_column(Integer, ForeignKey("question_type.id"), nullable=False)
+    type: Mapped["QuestionType"] = relationship("QuestionType", back_populates="questions", lazy="selectin")
 
     created_at: Mapped[datetime.datetime] = mapped_column(TIMESTAMP(timezone=True),
                                                           default=lambda: datetime.datetime.now(datetime.timezone.utc))
